51246cd58d6d9c5f8f46e33b568e458ab78c5403
Touhidul Islam <touhidul.islam@justice.gov.uk>
2022-01-10 12:45:46 +0000

    Add slack signature and hmac hexdigest

--- app/controllers/slack_controller.rb
+++ app/controllers/slack_controller.rb
@@ -3,16 +3,17 @@ class SlackController < ApplicationController
     slack_signing_secret = ENV["MY_SLACK_SIGNING_SECRET"]
     @challenge = params.require(:slack).permit(:challenge)
     timestamp = request.headers['X-Slack-Request-Timestamp']
-    request_body = request.body()
-    sig_basestring = 'v0:' + @timestamp + ':' + request_body
-    my_signature = 'v0=' + hmac.compute_hash_sha256(
-      slack_signing_secret,
-      sig_basestring
-      ).hexdigest()
+    request_body = request.body.to_json
+    sig_basestring = "v0:#{@timestamp}:#{request_body}"
+    digest = OpenSSL::Digest.new('sha256')
+    hmac_hexdigest = OpenSSL::HMAC.hexdigest(digest, slack_signing_secret, request_body)
+    my_signature = "v0=#{hmac_hexdigest}"
+    slack_signature = request.headers['X-Slack-Signature']
     puts @challenge
     puts timestamp
     puts slack_signing_secret
     puts request_body
     puts my_signature
+    puts slack_signature
   end
 end
