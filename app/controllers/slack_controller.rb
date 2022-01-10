e5438d04a412dcc39620bb4b0f0268fb682ca587
Touhidul Islam <touhidul.islam@justice.gov.uk>
2022-01-10 11:49:50 +0000

    Puts timestamp for trbl shotting

--- app/controllers/slack_controller.rb
+++ app/controllers/slack_controller.rb
@@ -1,6 +1,9 @@
 class SlackController < ApplicationController
   def events
+    byebug
     @challenge = params.require(:slack).permit(:challenge)
+    @timestamp = request.headers['X-Slack-Request-Timestamp']
     puts @challenge
+    puts @timestamp
   end
 end
