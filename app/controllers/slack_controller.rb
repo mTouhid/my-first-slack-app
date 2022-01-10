405b13c271b1f206dc4c681588d753f47b23deba
Touhidul Islam <touhidul.islam@justice.gov.uk>
2022-01-10 16:52:01 +0000

    Add env variable for slack webhook

--- app/controllers/slack_controller.rb
+++ app/controllers/slack_controller.rb
@@ -7,13 +7,13 @@ class SlackController < ApplicationController
 
   def events
     render plain: params.require(:slack).permit(:challenge)[:challenge]
-    send_message(params.require(:slack).permit(:challenge)[:challenge])
+    send_message
   end
 
   private
 
-  def send_message(message)
-    HTTP.post("https://hooks.slack.com/services/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", :json => {"type":"mrkdwn","text":"*Test Test Test*\n_2_"})
+  def send_message
+    HTTP.post(ENV['MY_SLACK_WEBHOOK_URL'], :json => {"type":"mrkdwn","text":"*Test Test Test*\n_2_"})
   end
 
 end
