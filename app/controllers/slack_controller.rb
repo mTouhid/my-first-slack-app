c06e92f3d40253c823277ac6f4539c0adf48d2fa
Touhidul Islam <touhidul.islam@justice.gov.uk>
2022-01-10 11:52:01 +0000

    Remove byebug

--- app/controllers/slack_controller.rb
+++ app/controllers/slack_controller.rb
@@ -1,6 +1,5 @@
 class SlackController < ApplicationController
   def events
-    byebug
     @challenge = params.require(:slack).permit(:challenge)
     @timestamp = request.headers['X-Slack-Request-Timestamp']
     puts @challenge
