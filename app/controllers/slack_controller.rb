c48850ee4516915715590ac5c15824143414260b
Touhidul Islam <touhidul.islam@justice.gov.uk>
2022-01-10 12:33:52 +0000

    Add signature

--- app/controllers/slack_controller.rb
+++ app/controllers/slack_controller.rb
@@ -1,12 +1,18 @@
 class SlackController < ApplicationController
   def events
-    @slack_signing_secret = ENV["MY_SLACK_SIGNING_SECRET"]
+    slack_signing_secret = ENV["MY_SLACK_SIGNING_SECRET"]
     @challenge = params.require(:slack).permit(:challenge)
-    @timestamp = request.headers['X-Slack-Request-Timestamp']
-    @request_body = request.body()
+    timestamp = request.headers['X-Slack-Request-Timestamp']
+    request_body = request.body()
+    sig_basestring = 'v0:' + @timestamp + ':' + request_body
+    my_signature = 'v0=' + hmac.compute_hash_sha256(
+      slack_signing_secret,
+      sig_basestring
+      ).hexdigest()
     puts @challenge
-    puts @timestamp
-    puts @slack_signing_secret
-    puts @request_body
+    puts timestamp
+    puts slack_signing_secret
+    puts request_body
+    puts my_signature
   end
 end
