378b232a170346b742e40d72969405a9608fa80c
Touhidul Islam <touhidul.islam@justice.gov.uk>
2022-01-10 12:25:45 +0000

    Add local variables

--- app/controllers/slack_controller.rb
+++ app/controllers/slack_controller.rb
@@ -1,8 +1,12 @@
 class SlackController < ApplicationController
   def events
+    @slack_signing_secret = ENV["MY_SLACK_SIGNING_SECRET"]
     @challenge = params.require(:slack).permit(:challenge)
     @timestamp = request.headers['X-Slack-Request-Timestamp']
+    @request_body = request.body()
     puts @challenge
     puts @timestamp
+    puts @slack_signing_secret
+    puts @request_body
   end
 end
