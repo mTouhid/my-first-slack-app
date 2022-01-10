54064bb26674bbc5279a32448854b612bf956233
Touhidul Islam <touhidul.islam@justice.gov.uk>
2022-01-10 12:57:44 +0000

    fix_up

--- app/controllers/slack_controller.rb
+++ app/controllers/slack_controller.rb
@@ -1,9 +1,11 @@
+require 'json'
+
 class SlackController < ApplicationController
   def events
     slack_signing_secret = ENV["MY_SLACK_SIGNING_SECRET"]
     @challenge = params.require(:slack).permit(:challenge)
     timestamp = request.headers['X-Slack-Request-Timestamp']
-    request_body = request.body.to_json
+    request_body = URI.encode_www_form(request.params)
     sig_basestring = "v0:#{@timestamp}:#{request_body}"
     digest = OpenSSL::Digest.new('sha256')
     hmac_hexdigest = OpenSSL::HMAC.hexdigest(digest, slack_signing_secret, request_body)
@@ -15,5 +17,10 @@ class SlackController < ApplicationController
     puts request_body
     puts my_signature
     puts slack_signature
+    if my_signature == slack_signature
+      puts "Yesssss!"
+    else
+      puts "Nooooooo!"
+    end
   end
 end
