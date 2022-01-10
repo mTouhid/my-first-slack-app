36667445e8f0bb258393da15ce74a8a66b0fb25d
Touhidul Islam <touhidul.islam@justice.gov.uk>
2022-01-10 15:52:26 +0000

    Try setting response

--- app/controllers/slack_controller.rb
+++ app/controllers/slack_controller.rb
@@ -2,27 +2,6 @@ require 'json'
 
 class SlackController < ApplicationController
   def events
-    # slack_signing_secret = ENV["MY_SLACK_SIGNING_SECRET"]
-    # @challenge = params.require(:slack).permit(:challenge)
-    # timestamp = request.headers['X-Slack-Request-Timestamp']
-    # request_body = URI.encode_www_form(request.params[:slack])
-    # sig_basestring = "v0:#{timestamp}:#{request_body}"
-    # digest = OpenSSL::Digest.new('sha256')
-    # hmac_hexdigest = OpenSSL::HMAC.hexdigest(digest, slack_signing_secret, request_body)
-    # my_signature = "v0=#{hmac_hexdigest}"
-    # slack_signature = request.headers['X-Slack-Signature']
-    # puts @challenge
-    # puts timestamp
-    # puts slack_signing_secret
-    # puts request_body
-    # puts sig_basestring
-    # puts my_signature
-    # puts slack_signature
-    # if my_signature == slack_signature
-    #   puts "Yesssss!"
-    # else
-    #   puts "Nooooooo!"
-    # end
     signing_secret = ENV['MY_SLACK_SIGNING_SECRET']
     version_number = 'v0' # always v0 for now
     timestamp = request.headers['X-Slack-Request-Timestamp']
@@ -43,7 +22,7 @@ class SlackController < ApplicationController
     if computed_signature != slack_signature
       render nothing: true, status: :unauthorized
     else
-      puts "Yessssssss!"
+      @request = params.require(:slack).permit(:slack)
     end
   end
 end
