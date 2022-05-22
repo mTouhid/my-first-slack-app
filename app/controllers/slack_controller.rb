ab8f1a767b793ba4d589b322417498675e938b41
Touhidul Islam <touhidul.islam@justice.gov.uk>
2022-05-22 23:50:11 +0100

    respose id

--- app/controllers/slack_controller.rb
+++ app/controllers/slack_controller.rb
@@ -33,6 +33,7 @@ class SlackController < ApplicationController
 
   def touhid
     render plain: modal
+    HTTP.auth("Bearer #{ENV['MY_OAUTH_TOKEN']}").post("https://slack.com/api/views.open", :json => {"channel":"C02TX2LNSQG","trigger_id":params[:trigger_id],"view":modal})
     HTTP.auth("Bearer #{ENV['MY_OAUTH_TOKEN']}").post("https://slack.com/api/chat.postMessage", :json => {"channel":"C02TX2LNSQG","text":"Hi @Touhidul Islam"})
   end
 
